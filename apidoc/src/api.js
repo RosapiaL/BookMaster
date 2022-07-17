7c3a9e68f4360d8caf8c589fadeaaf33a436f831
Rosapia Laudati <rosilaudati@gmail.com>
2022-07-17 18:17:12 +0200

    404NotFound

--- apidoc/src/api.js
+++ apidoc/src/api.js
@@ -54,13 +54,13 @@
  * @apiError attribute_not_declared title not declared
  *
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *     {'error':"attribute title not declared"}
  *
  * @apiError database_offline database of this book not exist
  *
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *      {
  *          "title": "Harry Potter e la Pietra Filosofale",
  *          "picture": "http://books.google.com/books/content?id=9CJWTbd-RYoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
@@ -72,7 +72,7 @@
  * @apiError No_books_for_the_title_searched If you search a book that is not found in google books database
  *
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *     {'error':"No books with this title"}
  * 
  * 
@@ -135,13 +135,13 @@
  * @apiError attribute_not_declared title not declared
  *
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *     {'error':"attribute id not declared"}
  * 
 * @apiError database_offline database of this book not exist
  *
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *      {
  *          "title": "Harry Potter e la Pietra Filosofale",
  *          "picture": "http://books.google.com/books/content?id=9CJWTbd-RYoC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE73rVE-11HuoHjzZ-Hl74nMpEy-sEtRNXf1E2tXPN-QXVWM26WvsP5lX41ioBOXkaAsJ9fOqu5yS8W4_1AvlbB_tOJJ1lGOTEqJj8r6EX52Gerc3tDCpDOvS4gmaFF4jHmr1fcrB&source=gbs_api",
@@ -152,7 +152,7 @@
  *@apiError No_book_found_for_the_id_in_the_url  if you are searching for a book whit id that does not exists in the google book db
  *
  *@apiErrorExample Error-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 404 Not Found
  *     {'error':"No book for this id in google books"}
  * 
  */
